57367f6b7201e425bb4f3844ec759d329e706d36
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-09-13 14:25:16 +0900

    🗑️ bin
    - so you can run binary

---
+++ cli/bin/prompt-piper.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+// Check if we're in development or production
+const isDev = require('fs').existsSync(path.join(__dirname, '../src'));
+
+if (isDev) {
+  // Development: use ts-node
+  require('ts-node/register');
+  require(path.join(__dirname, '../src/index.ts'));
+} else {
+  // Production: use compiled JS
+  require(path.join(__dirname, '../dist/index.js'));
+}
