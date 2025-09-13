949e03452f03ac2a131112eee773132930554397
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-09-13 23:26:40 +0900

    fix imports

--- cli/src/tiktoken-utils.ts
+++ cli/src/tiktoken-utils.ts
@@ -1,6 +1,7 @@
 // Tiktoken utilities for accurate token counting
 // Export o200k_base ranks to avoid ESM import issues
 
-import o200k_base from 'js-tiktoken/ranks/o200k_base';
+// Use require for CommonJS compatibility
+const o200k_base = require('js-tiktoken/ranks/o200k_base');
 
 export { o200k_base };
